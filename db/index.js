b391cb71bfff26b3c4f0359abad937400988dafa
jecinacDarko <darkojecinac@gmail.com>
2022-10-17 01:31:39 +0200

    working on it

--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
+require('dotenv').config({path:'../.env'});
+
 const { MongoClient } = require("mongodb");
-
-const uri =
-  "mongodb+srv://jecinacDarko:bellatrixM42@mycluster.zmis2lc.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = process.env.API_KEY;
 const client = new MongoClient(uri);
 
 async function run() {
@@ -12,7 +11,7 @@ async function run() {
     const projects = collection.find({});
     const projectsArray = await projects.toArray();
 
-    console.log(projectsArray);
+     console.log(projectsArray);
   } finally {
 
     await client.close();
