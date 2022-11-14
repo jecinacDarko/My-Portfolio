ddc0fe16993287f088cd8b95f99f05b0e505029f
jecinacDarko <darkojecinac@gmail.com>
2022-11-14 06:13:27 +0100

    working on it

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,12 @@
 import './Projects.css';
 import projectCardData from '../mock-data/ProjectCardData';
 import WorkCard from './WorkCard';
+import motion from "framer-motion";
 
 export default function Projects() {
   return (
     <div className='projectsPage'>
+      
       <h1 className='projectsHeadline'>Latest Projects.</h1>
       <p className='projectsPage_title'>Check out my recent work</p>
       <div className="projectsContainer">
