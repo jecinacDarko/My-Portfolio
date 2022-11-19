4ea20ea9119775ca3dacaa2eee5f6a8ac880210f
jecinacDarko <darkojecinac@gmail.com>
2022-11-19 16:52:09 +0100

    working on new styling

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -18,23 +18,25 @@ export const Form = () => {
   };
 
   return (
-    <div className='formContainer' id="contact">
-      <h1>Contact</h1>
-      <p>Feel free to send me an email!</p>
-      {!messageSent && <div className="form">
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Your Name</label>
-          <input type="text" name="user_name"></input>
-          <label>Email</label>
-          <input type="email" name="user_email"></input>
-          <label>Subject</label>
-          <input type="text" name="subject"></input> 
-          <label>Message</label>
-          <textarea rows="6" placeholder="Type your message here" name="message"/>
-          <button className="btn" type="submit" value="send">Submit</button> 
-        </form>
-      </div>}
-      {messageSent && <div className="messageSent">Thank you for contacting me! <br></br>Your email has been sent sucessfully! </div>}
+    <div className='formSection' id="contact">
+      <div className='formContainer'>
+        <h1>Contact</h1>
+        <p>Feel free to send me an email!</p>
+        {!messageSent && <div className="form">
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Your Name</label>
+            <input type="text" name="user_name"></input>
+            <label>Email</label>
+            <input type="email" name="user_email"></input>
+            <label>Subject</label>
+            <input type="text" name="subject"></input> 
+            <label>Message</label>
+            <textarea rows="6" placeholder="Type your message here" name="message"/>
+            <button className="btn" type="submit" value="send">Submit</button> 
+          </form>
+        </div>}
+        {messageSent && <div className="messageSent">Thank you for contacting me! <br></br>Your email has been sent sucessfully! </div>}
+      </div>
     </div>  
   );
 };
