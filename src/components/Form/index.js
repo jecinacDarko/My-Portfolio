6807d5bfe0e278cc07f9dac7c4d29ef051ea6438
jecinacDarko <darkojecinac@gmail.com>
2022-12-01 01:28:37 +0100

    quick_fix

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -25,13 +25,13 @@ export const Form = () => {
         {!messageSent && <div className="form">
           <form ref={form} onSubmit={sendEmail}>
             <label>Your Name</label>
-            <input type="text" name="user_name"></input>
+            <input type="text" name="user_name" required></input>
             <label>Email</label>
-            <input type="email" name="user_email"></input>
+            <input type="email" name="user_email" required></input>
             <label>Subject</label>
-            <input type="text" name="subject"></input> 
+            <input type="text" name="subject" required></input> 
             <label>Message</label>
-            <textarea rows="6" placeholder="Type your message here" name="message"/>
+            <textarea rows="6" placeholder="Type your message here" name="message" required/>
             <button className="btn" type="submit" value="send">Submit</button> 
           </form>
         </div>}
